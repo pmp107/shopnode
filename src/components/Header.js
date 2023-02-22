e53dab4e8a085565b9e89b7a202507ff1321c811
pmp107 <pmp441996@gmail.com>
2023-02-22 16:37:23 +0530

    changed logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom"; 
-import Logo from "../assets/logo.png"
+import Logo from "../assets/pmplogo2.svg"
 import { useCart } from "../context/CartContext";
 import "./Header.css";
 
